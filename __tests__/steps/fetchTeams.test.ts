f0f861e2f7bc3496eae6d1689e47ef37b57a5610
Nick Dowmon <nick.dowmon@lifeomic.com>
2020-04-18 00:18:56 -0400

    ingest /teams api

---
+++ __tests__/steps/fetchTeams.test.ts
@@ -0,0 +1,189 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { createMockStepExecutionContext } from '@jupiterone/integration-sdk/testing';
+import { Polly } from '@pollyjs/core';
+import NodeHTTPAdapter = require('@pollyjs/adapter-node-http');
+import { v4 as uuid } from 'uuid';
+import step from '../../src/steps/fetchTeams';
+
+const instanceConfig = {
+  apiKey: 'api-key',
+};
+
+Polly.register(NodeHTTPAdapter);
+let polly: Polly;
+
+beforeEach(() => {
+  polly = new Polly('api.heroku.com', {
+    adapters: ['node-http'],
+  });
+});
+
+afterEach(() => {
+  polly.stop();
+});
+
+describe('executionHandler', () => {
+  test('should add teams returned by /teams', async () => {
+    const responseBody = [
+      {
+        id: uuid(),
+        name: 'name',
+        created_at: 'created_at',
+        updated_at: 'updated_at',
+        default: true,
+        credit_card_collections: true,
+        enterprise_account: {
+          id: uuid(),
+          name: 'enterprise_account_name',
+        },
+        membership_limit: 100,
+        identity_provider: {
+          id: uuid(),
+          name: 'name',
+          owner: {
+            id: uuid(),
+            name: 'name',
+            type: 'type',
+          },
+        },
+        provisioned_licenses: true,
+        role: 'role',
+        type: 'type',
+      },
+    ];
+
+    polly.server
+      .get('https://api.heroku.com/teams')
+      .intercept((req, res) => res.status(200).json(responseBody));
+
+    const context = createMockStepExecutionContext({ instanceConfig });
+    const addEntities = jest.spyOn(context.jobState, 'addEntities');
+    const addRelationships = jest.spyOn(context.jobState, 'addRelationships');
+    await step.executionHandler(context);
+
+    expect(addEntities).toHaveBeenCalledTimes(1);
+    expect(addEntities).toHaveBeenCalledWith([
+      {
+        _class: ['Team'],
+        _key: responseBody[0].id,
+        _rawData: [
+          {
+            name: 'default',
+            rawData: {
+              createdAt: 'created_at',
+              creditCardCollections: true,
+              default: true,
+              enterpriseAccountId: responseBody[0].enterprise_account.id,
+              id: responseBody[0].id,
+              identityProviderId: responseBody[0].identity_provider.id,
+              membershipLimit: 100,
+              name: 'name',
+              provisionedLicenses: true,
+              type: 'type',
+              role: 'role',
+              updatedAt: 'updated_at',
+            },
+          },
+        ],
+        _type: 'heroku_team',
+        createdOn: undefined,
+        displayName: 'name',
+        id: responseBody[0].id,
+        name: 'name',
+      },
+    ]);
+
+    expect(addRelationships).toHaveBeenCalledTimes(1);
+    expect(addRelationships).toHaveBeenCalledWith([
+      {
+        _class: 'HAS',
+        _fromEntityKey: responseBody[0].enterprise_account.id,
+        _key: `${responseBody[0].enterprise_account.id}|has|${responseBody[0].id}`,
+        _toEntityKey: responseBody[0].id,
+        _type: 'heroku_account_has_team',
+        displayName: 'HAS',
+      },
+    ]);
+  });
+
+  test('should add teams with null fields returned by /teams', async () => {
+    const responseBody = [
+      {
+        id: uuid(),
+        name: 'name',
+        created_at: null,
+        updated_at: null,
+        default: null,
+        credit_card_collections: null,
+        enterprise_account: null,
+        membership_limit: null,
+        identity_provider: null,
+        provisioned_licenses: null,
+        role: null,
+        type: null,
+      },
+    ];
+
+    polly.server
+      .get('https://api.heroku.com/teams')
+      .intercept((req, res) => res.status(200).json(responseBody));
+
+    const context = createMockStepExecutionContext({ instanceConfig });
+    const addEntities = jest.spyOn(context.jobState, 'addEntities');
+    const addRelationships = jest.spyOn(context.jobState, 'addRelationships');
+    await step.executionHandler(context);
+
+    expect(addEntities).toHaveBeenCalledTimes(1);
+    expect(addEntities).toHaveBeenCalledWith([
+      {
+        _class: ['Team'],
+        _key: responseBody[0].id,
+        _rawData: [
+          {
+            name: 'default',
+            rawData: {
+              createdAt: null,
+              creditCardCollections: null,
+              default: null,
+              enterpriseAccountId: null,
+              id: responseBody[0].id,
+              identityProviderId: null,
+              membershipLimit: null,
+              name: 'name',
+              provisionedLicenses: null,
+              type: null,
+              role: null,
+              updatedAt: null,
+            },
+          },
+        ],
+        _type: 'heroku_team',
+        createdOn: undefined,
+        displayName: 'name',
+        id: responseBody[0].id,
+        name: 'name',
+      },
+    ]);
+
+    expect(addRelationships).toHaveBeenCalledTimes(1);
+    expect(addRelationships).toHaveBeenCalledWith([]);
+  });
+
+  test('should add nothing if no teams returned by /teams', async () => {
+    const responseBody = [];
+
+    polly.server
+      .get('https://api.heroku.com/teams')
+      .intercept((req, res) => res.status(200).json(responseBody));
+
+    const context = createMockStepExecutionContext({ instanceConfig });
+    const addEntities = jest.spyOn(context.jobState, 'addEntities');
+    const addRelationships = jest.spyOn(context.jobState, 'addRelationships');
+    await step.executionHandler(context);
+
+    expect(addEntities).toHaveBeenCalledTimes(1);
+    expect(addEntities).toHaveBeenCalledWith([]);
+    expect(addRelationships).toHaveBeenCalledTimes(1);
+    expect(addRelationships).toHaveBeenCalledWith([]);
+  });
+});
