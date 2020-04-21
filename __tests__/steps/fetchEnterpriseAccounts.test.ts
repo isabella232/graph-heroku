62eacfe393cdac7f623581acc434729f1d8f76f2
Nick Dowmon <nick.dowmon@lifeomic.com>
2020-04-21 10:05:48 -0400

    use sdk Recording to simplify enterprise account tests

--- __tests__/steps/fetchEnterpriseAccounts.test.ts
+++ __tests__/steps/fetchEnterpriseAccounts.test.ts
@@ -1,147 +1,54 @@
 /* eslint-disable @typescript-eslint/camelcase */
-import { createMockStepExecutionContext } from '@jupiterone/integration-sdk/testing';
-import { Polly } from '../../test/polly';
-import { v4 as uuid } from 'uuid';
+import {
+  createMockStepExecutionContext,
+  setupRecording,
+  Recording,
+} from '@jupiterone/integration-sdk/testing';
 import step from '../../src/steps/fetchEnterpriseAccounts';
 
-let polly: Polly;
-
-const instanceConfig = {
-  apiKey: 'api-key',
-};
-
-beforeEach(() => {
-  polly = new Polly('api.heroku.com', {
-    adapters: ['node-http'],
-  });
-});
+let recording: Recording;
 
 afterEach(() => {
-  polly.stop();
+  recording.stop();
 });
 
 describe('executionHandler', () => {
   test('should add accounts returned by /enterprise-accounts', async () => {
-    const responseBody = [
-      {
-        id: uuid(),
-        name: 'name',
-        created_at: 'created_at',
-        updated_at: 'updated_at',
-        permissions: ['1', '2'],
-        trial: true,
-        identity_provider: {
-          id: uuid(),
-          name: 'name',
-          owner: {
-            id: uuid(),
-            name: 'name',
-            type: 'type',
-          },
-        },
-      },
-    ];
+    recording = setupRecording({
+      name: 'add-accounts',
+      directory: __dirname,
+      redactedRequestHeaders: ['authorization'],
+    });
 
-    polly.server
-      .get('https://api.heroku.com/enterprise-accounts')
-      .intercept((req, res) => {
-        res.status(200).json(responseBody);
-      });
-
-    const context = createMockStepExecutionContext({ instanceConfig });
+    const context = createMockStepExecutionContext();
     await step.executionHandler(context);
 
-    expect(context.jobState.collectedEntities).toMatchObject([
-      {
+    expect(context.jobState.collectedEntities).toEqual([
+      expect.objectContaining({
         _class: ['Account'],
-        _key: responseBody[0].id,
-        _rawData: [
-          {
-            name: 'default',
-            rawData: {
-              createdAt: 'created_at',
-              id: responseBody[0].id,
-              identityProviderId: responseBody[0].identity_provider.id,
-              name: 'name',
-              permissions: '1,2',
-              trial: true,
-              updatedAt: 'updated_at',
-            },
-          },
-        ],
+        _key: '01234567-89ab-cdef-0123-456789abcdef',
         _type: 'heroku_account',
-        createdOn: undefined,
-        displayName: 'name',
-        id: responseBody[0].id,
-        name: 'name',
-      },
+        createdOn: 1325419200000,
+        displayName: 'example',
+        id: '01234567-89ab-cdef-0123-456789abcdef',
+        name: 'example',
+      }),
     ]);
-    expect(context.jobState.collectedRelationships).toMatchObject([]);
-  });
 
-  test('should add accounts with null fields returned by /enterprise-accounts', async () => {
-    const responseBody = [
-      {
-        id: uuid(),
-        name: 'name',
-        created_at: null,
-        updated_at: null,
-        permissions: null,
-        trial: null,
-        identity_provider: null,
-      },
-    ];
-
-    polly.server
-      .get('https://api.heroku.com/enterprise-accounts')
-      .intercept((req, res) => {
-        res.status(200).json(responseBody);
-      });
-
-    const context = createMockStepExecutionContext({ instanceConfig });
-    await step.executionHandler(context);
-
-    expect(context.jobState.collectedEntities).toMatchObject([
-      {
-        _class: ['Account'],
-        _key: responseBody[0].id,
-        _rawData: [
-          {
-            name: 'default',
-            rawData: {
-              createdAt: null,
-              id: responseBody[0].id,
-              identityProviderId: null,
-              name: 'name',
-              permissions: null,
-              trial: null,
-              updatedAt: null,
-            },
-          },
-        ],
-        _type: 'heroku_account',
-        createdOn: undefined,
-        displayName: 'name',
-        id: responseBody[0].id,
-        name: 'name',
-      },
-    ]);
-    expect(context.jobState.collectedRelationships).toMatchObject([]);
+    expect(context.jobState.collectedRelationships).toEqual([]);
   });
 
   test('should add nothing if no accounts returned by /enterprise-accounts', async () => {
-    const responseBody = [];
+    recording = setupRecording({
+      name: 'no-accounts',
+      directory: __dirname,
+      redactedRequestHeaders: ['authorization'],
+    });
 
-    polly.server
-      .get('https://api.heroku.com/enterprise-accounts')
-      .intercept((req, res) => {
-        res.status(200).json(responseBody);
-      });
-
-    const context = createMockStepExecutionContext({ instanceConfig });
+    const context = createMockStepExecutionContext();
     await step.executionHandler(context);
 
-    expect(context.jobState.collectedEntities).toMatchObject([]);
-    expect(context.jobState.collectedRelationships).toMatchObject([]);
+    expect(context.jobState.collectedEntities).toEqual([]);
+    expect(context.jobState.collectedRelationships).toEqual([]);
   });
 });
