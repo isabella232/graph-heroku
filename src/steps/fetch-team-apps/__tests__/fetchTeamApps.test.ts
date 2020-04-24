ec21ec01ec1e01ccf3b647a4d9b21f92080e7939
Nick Dowmon <nick.dowmon@lifeomic.com>
2020-04-24 15:01:53 -0400

    use partialApp constant in fetchTeamApps test

--- src/steps/fetch-team-apps/__tests__/fetchTeamApps.test.ts
+++ src/steps/fetch-team-apps/__tests__/fetchTeamApps.test.ts
@@ -26,34 +26,36 @@ afterEach(() => {
   recording.stop();
 });
 
+const partialApp = {
+  acm: true,
+  created_at: '2020-04-24T17:53:13Z',
+  git_url: 'https://git.heroku.com/ndowmon-gmail-test-app.git',
+  id: '2a089907-da73-49d6-b569-5f4a89c65ba9',
+  name: 'ndowmon-gmail-test-app',
+  organization: {
+    id: '1e2fd576-c276-483d-8440-3c956e0fe015',
+    name: 'ndowmon-lifeomic-test',
+  },
+  owner: {
+    email: 'ndowmon-lifeomic-test@herokumanager.com',
+    id: '1e2fd576-c276-483d-8440-3c956e0fe015',
+  },
+  region: {
+    id: '59accabd-516d-4f0e-83e6-6e3757701145',
+    name: 'us',
+  },
+  released_at: '2020-04-24T17:53:13Z',
+  updated_at: '2020-04-24T18:18:26Z',
+  web_url: 'https://ndowmon-gmail-test-app.herokuapp.com/',
+};
+
 test('should fetch members', async () => {
   const context = createMockStepExecutionContext({ entities });
   const heroku = new HerokuClient(context.instance.config);
   const results = await heroku.getTeamApps(entities[0].id);
 
   expect(results).toEqual([
-    expect.objectContaining({
-      acm: true,
-      created_at: '2020-04-24T17:53:13Z',
-      git_url: 'https://git.heroku.com/ndowmon-gmail-test-app.git',
-      id: '2a089907-da73-49d6-b569-5f4a89c65ba9',
-      name: 'ndowmon-gmail-test-app',
-      organization: {
-        id: '1e2fd576-c276-483d-8440-3c956e0fe015',
-        name: 'ndowmon-lifeomic-test',
-      },
-      owner: {
-        email: 'ndowmon-lifeomic-test@herokumanager.com',
-        id: '1e2fd576-c276-483d-8440-3c956e0fe015',
-      },
-      region: {
-        id: '59accabd-516d-4f0e-83e6-6e3757701145',
-        name: 'us',
-      },
-      released_at: '2020-04-24T17:53:13Z',
-      updated_at: '2020-04-24T18:18:26Z',
-      web_url: 'https://ndowmon-gmail-test-app.herokuapp.com/',
-    }),
+    expect.objectContaining(partialApp),
     expect.objectContaining({
       acm: true,
       created_at: '2020-04-14T13:23:01Z',
@@ -80,7 +82,7 @@ test('should fetch members', async () => {
 });
 
 test('should convert members to entities', () => {
-  const entity = createApplicationEntity(partialApp1, entities[0].id);
+  const entity = createApplicationEntity(partialApp, entities[0].id);
 
   expect(entity).toMatchObject({
     _class: ['Application'],
